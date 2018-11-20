bfbf5a46da885c9ab3732215072ce65f6c2241f8
Felix <2680383605@qq.com>
2018-11-20 15:07:47 +0800

    ⚙️ fix bug

--- src/lib/webgl2/shader.ts
+++ src/lib/webgl2/shader.ts
@@ -1,5 +1,5 @@
 function loadShader(gl: WebGLRenderingContext, type: number, source: string): WebGLShader | null {
-  const shader = gl.createShader(type)
+  const shader = gl.createShader(type)!
   gl.shaderSource(shader, source)
   gl.compileShader(shader)
 
@@ -13,10 +13,10 @@ function loadShader(gl: WebGLRenderingContext, type: number, source: string): We
 }
 
 export function initShaderProgram(gl: WebGLRenderingContext, vsSource: string, fsSource: string): WebGLProgram | null {
-  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource)
-  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource)
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource)!
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource)!
 
-  const shaderProgram = gl.createProgram()
+  const shaderProgram = gl.createProgram()!
   gl.attachShader(shaderProgram, vertexShader)
   gl.attachShader(shaderProgram, fragmentShader)
   gl.linkProgram(shaderProgram)
